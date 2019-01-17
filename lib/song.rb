2425351d302a4a5f2656c90e56db12c65c95a5ff
Steve Albonico <salbonico@gmail.com>
2019-01-17 18:37:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ def self.create_by_name(name)
 end
 
 def self.find_by_name(name)
-  self.class.all.each do |object|
+  self.all.each do |object|
     if object.name == name
       return object
     end
