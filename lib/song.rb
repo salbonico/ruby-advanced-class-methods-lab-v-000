cb343a9580c70e7f6a28d945e2f13f51fdd4b0c9
Steve Albonico <salbonico@gmail.com>
2019-01-17 18:41:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,10 +40,10 @@ return false
 end
 
 def self.find_or_create_by_name(name)
-  if self.find_by_name == false
-    self.create_by_name
+  if self.find_by_name(name) == false
+    self.create_by_name(name)
   else
-    self.find_by_name
+    self.find_by_name(name)
   end
 end
 
